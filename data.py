b1b21c4b02697266ee00c549e25d1c44e31eac94
The Game <38539205+jozsef123@users.noreply.github.com>
2019-08-05 19:41:47 -0700

    Store sign-in/email info here

--- data.py
+++ data.py
@@ -7,6 +7,7 @@ gmailPass = "password"
 receiver = "fake@gmail.com"
 '''
 
+# Replace each string with your data
 username = "delete this and type your student ID number"
 password = "delete this and type your student ID password"
 gmailUser = "who is sending the email"
