bb6b81e169b55d7becbbbf0df3dd91403d8bec04
The Game <38539205+jozsef123@users.noreply.github.com>
2019-08-05 19:25:58 -0700

    Create data.py

---
+++ data.py
@@ -0,0 +1,14 @@
+# example
+'''
+username = "8844523432"
+password = "password"
+gmailUser = "bob@gmail.com"
+gmailPass = "password"
+receiver = "fake@gmail.com"
+'''
+
+username = "delete this and type your student ID number"
+password = "delete this and type your student ID password"
+gmailUser = "who is sending the email"
+gmailPass = "password of the emailer"
+receiver = "who is receiving the email"
